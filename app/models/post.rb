7eeffcade45cb68b37f72e5130e15dcea654e5cb
wayungi <fwayungi@gmail.com>
2022-10-21 20:29:34 +0300

    ADD: get 5 recent comments method

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,13 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   has_many :comments
   has_many :likes
+
+
+  # A method that updates the posts counter for a user.
+
+  # A method which returns the 5 most recent comments for a given post.
+  def get_5_recent_comments
+    comments.order(:created_at).first(5)
+  end
+
 end
