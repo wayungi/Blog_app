bb8a6dd25aa17e759cb45023126202bfdc06e58e
wayungi <fwayungi@gmail.com>
2022-10-21 20:23:08 +0300

    ADD: get 3 recent posts method

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,11 @@ class User < ApplicationRecord
     has_many :posts, foreign_key: 'author_id'
     has_many :comments, foreign_key: 'author_id'
     has_many :likes, foreign_key: 'author_id'
+
+
+   # A method that returns the 3 most recent posts for a given user.
+   def get_three_recent_posts
+    posts.order(:created_at).first(3)
+   end
+
 end
