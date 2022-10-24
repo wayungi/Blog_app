e45b1d97ae679e107872cef8aafe8b8cad018e37
wayungi <fwayungi@gmail.com>
2022-10-24 17:54:09 +0300

    DELETED: blank user route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   get 'user/users'
-  get 'user/user'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
