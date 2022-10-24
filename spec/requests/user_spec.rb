459c60ce7b38d2512c5da613bffc2c4553dcc785
wayungi <fwayungi@gmail.com>
2022-10-24 23:06:09 +0300

    ADDED: tests for status code & body text

--- spec/requests/user_spec.rb
+++ spec/requests/user_spec.rb
@@ -1,10 +1,43 @@
 require 'rails_helper'
 
+
+# If response status was correct.
+# If a correct template was rendered.
+# If the response body includes correct placeholder text.
+
+
+
 RSpec.describe 'Users', type: :request do
-  describe 'GET /users' do
-    it 'returns http success' do
-      get '/user/users'
-      expect(response).to have_http_status(:success)
+    describe 'GET /users' do
+      # tests correct response status
+      it 'returns http success' do
+        get '/user/users'
+        expect(response).to have_http_status(:success)
+      end
+
+      # Tests a correct template was rendered.
+      it 'returns http success' do
+        get '/user/users'
+        #expect(response).to render_template(:users)
+        expect(response).to render_template('users')
+        # expect(response).to have_http_status(:success)
+      end
+
+      # Test if response body includes correct placeholder text.
+      it 'returns http success' do
+        get '/user/users'
+        expect(response.body).to include 'List of users goes here'
+      end
+
     end
   end
-end
+  
+
+# RSpec.describe 'Users', type: :request do
+#   describe 'GET /users' do
+#     it 'returns http success' do
+#       get '/user/users'
+#       expect(response).to have_http_status(:success)
+#     end
+#   end
+# end
