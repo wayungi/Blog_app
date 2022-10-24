826e5617089a3409757f30ea624091e8eb6bc315
wayungi <fwayungi@gmail.com>
2022-10-24 23:21:03 +0300

    ADDED: tests for get user/user route

--- spec/requests/user_spec.rb
+++ spec/requests/user_spec.rb
@@ -16,19 +16,38 @@ RSpec.describe 'Users', type: :request do
       end
 
       # Tests a correct template was rendered.
-      it 'returns http success' do
+      it 'return the users template' do
         get '/user/users'
-        #expect(response).to render_template(:users)
         expect(response).to render_template('users')
-        # expect(response).to have_http_status(:success)
       end
 
       # Test if response body includes correct placeholder text.
-      it 'returns http success' do
+      it 'List of users goes here' do
         get '/user/users'
         expect(response.body).to include 'List of users goes here'
       end
+    end
 
+
+
+    describe 'GET /user' do
+      # tests correct response status
+      it 'returns http success' do
+        get '/user/users'
+        expect(response).to have_http_status(:success)
+      end
+
+      # Tests a correct template was rendered.
+      it 'returns the user template' do
+        get '/user/user'
+        expect(response).to render_template('user')
+      end
+
+      # Test if response body includes correct placeholder text.
+      it 'returns Show a single user with id' do
+        get '/user/user'
+        expect(response.body).to include 'Show a single user with id'
+      end
     end
   end
   
