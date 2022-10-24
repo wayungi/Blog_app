f913400368602d7fff28201758d0a401b31cc258
wayungi <fwayungi@gmail.com>
2022-10-24 23:32:26 +0300

    ADDED: tests for user/:id/posts route

--- spec/requests/user_spec.rb
+++ spec/requests/user_spec.rb
@@ -8,7 +8,7 @@ require 'rails_helper'
 
 
 RSpec.describe 'Users', type: :request do
-    describe 'GET /users' do
+    describe 'GET user/users' do
       # tests correct response status
       it 'returns http success' do
         get '/user/users'
@@ -28,9 +28,7 @@ RSpec.describe 'Users', type: :request do
       end
     end
 
-
-
-    describe 'GET /user' do
+    describe 'GET user/user' do
       # tests correct response status
       it 'returns http success' do
         get '/user/users'
@@ -47,6 +45,27 @@ RSpec.describe 'Users', type: :request do
       it 'returns Show a single user with id' do
         get '/user/user'
         expect(response.body).to include 'Show a single user with id'
+      end
+    end
+
+
+    describe 'GET user/user_posts' do
+      # tests correct response status
+      it 'returns http success' do
+        get '/user/10/posts'
+        expect(response).to have_http_status(:success)
+      end
+
+      # Tests a correct template was rendered.
+      it 'returns the user_posts template' do
+        get '/user/10/posts'
+        expect(response).to render_template('user_posts')
+      end
+
+      # Test if response body includes correct placeholder text.
+      it 'returns Show a single user with id' do
+        get '/user/10/posts'
+        expect(response.body).to include 'Show all posts of a single user'
       end
     end
   end
