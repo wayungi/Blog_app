c66abbb9174d8fdb9e4f9c9e3cb0b5b06fc81870
wayungi <fwayungi@gmail.com>
2022-10-26 00:32:07 +0300

    ADDED: display posts & commnets in detail, username of commenter against comment

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -4,6 +4,7 @@ class UserController < ApplicationController
     @users = User.all
   end
 
+  # Show  a user details with 3 most rescent comments
   def user
     @id = params[:id]
     @user = User.find_by(id: @id)
@@ -11,11 +12,20 @@ class UserController < ApplicationController
 
   end
 
+  # show all of the posts of a user in summary with thier comments
   def user_posts
     @id = params[:id]
     @user = User.find_by(id: @id)
     @posts = @user.all_posts
   end
 
-  def user_post; end
+  # show a single post in full with all its comments
+  def user_post
+    @user_id = params[:user_id]
+    @post_id = params[:id]
+
+    @user = User.find_by(id: @user_id)
+    @post = @user.posts.find_by(id:@post_id)
+    @comments = @post.comments
+  end
 end
