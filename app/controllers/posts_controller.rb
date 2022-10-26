f5e32096158753c555c814980cc7696bab443365
wayungi <fwayungi@gmail.com>
2022-10-26 18:22:07 +0300

    ADDED: posting new posts to the database

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,4 +4,15 @@ class PostsController < ApplicationController
     @user = User.new
     @current_user = @user.current_user
   end
+
+  def create
+    @post = Post.new(
+      author_id: params[:post][:author_id],
+      title: params[:post][:title],
+      text: params[:post][:text],
+      comments_counter: params[:post][:comments_counter],
+      likes_counter: params[:post][:likes_counter]
+    )
+    @post.save
+  end
 end
