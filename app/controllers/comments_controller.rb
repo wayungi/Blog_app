be601208cb250d27ecb9c6647444a53afd4fa218
wayungi <fwayungi@gmail.com>
2022-10-27 00:21:33 +0300

    ADDED: comment form

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,4 +5,13 @@ class CommentsController < ApplicationController
     @current_user = @user.current_user
     @post_id = 1 # This part should be revised
   end
+
+  def create 
+    @comment = Comment.new(
+      author_id: params[:comment][:author_id],
+      post_id: params[:comment][:post_id],
+      text: params[:comment][:text]
+    )
+    @comment.save
+  end
 end
