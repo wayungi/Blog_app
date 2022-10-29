679bf25e41e92be80bcaedf3282019bf2d564da5
wayungi <fwayungi@gmail.com>
2022-10-29 08:29:36 +0300

    ADDED: LikesController

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,10 @@
+class LikesController < ApplicationController
+
+    # handle creating new like
+    def create 
+    end
+
+    # handles unliking
+    def destroy 
+    end
+end
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,5 +1,5 @@
 class Like < ApplicationRecord
-  validates :author_id, uniqueness: {scope : post_id}
+  validates :post_id, uniqueness: {scope : author_id}
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   belongs_to :post, counter_cache: :likes_counter
 end
