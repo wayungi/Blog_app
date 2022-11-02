bf29b663e53c128c6e56703bf636267f91f80406
wayungi <fwayungi@gmail.com>
2022-11-02 18:27:57 +0300

    REFACTORED: user_post method

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   def index
-    @posts = posts.comments.order(:created_at).first(5)
+    # @posts = posts.comments.order(:created_at).first(5)
   end
 
   def new
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,18 +12,14 @@ class UsersController < ApplicationController
 
   # show all of the posts of a user in summary with thier comments
   def user_posts
-    @id = params[:id]
-    @user = User.find_by(id: @id)
+    @user = User.find(params[:id])
     @posts = @user.all_posts
   end
 
   # show a single post in full with all its comments
   def user_post
-    @user_id = params[:user_id]
-    @post_id = params[:id]
-
-    @user = User.find_by(id: @user_id)
-    @post = @user.posts.find_by(id: @post_id)
+    @user = User.find(params[:id])
+    @post = @user.posts.find(params[:id])
     @comments = @post.comments
   end
 end
