1c59b2eee5ba0116a0c5275e4551884160dc8f2e
wayungi <fwayungi@gmail.com>
2022-11-03 18:49:54 +0300

    ADDED: create a post on current user's behalf

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,13 @@
 class PostsController < ApplicationController
   def new
     @post = Post.new
-    @user = User.new
-    @current_user = @user.current_user
+    # @user = User.new
+    # @current_user = @user.current_user
   end
 
   def create
     @post = Post.new(post_params)
+    @post.author = current_user
     if @post.save
       redirect_to
     else
@@ -17,6 +18,6 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:author_id, :title, :text, :comments_counter, :likes_counter)
+    params.require(:post).permit(:title, :text)
   end
 end
