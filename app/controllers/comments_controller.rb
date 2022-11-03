da5259df29a9f4618fbaba32b880ca94aa86d077
wayungi <fwayungi@gmail.com>
2022-11-03 22:03:41 +0300

    UPDATE: add comment implementation

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,22 +1,29 @@
 class CommentsController < ApplicationController
   def new
     @comment = Comment.new
-    @user = User.new
-    @current_user = @user.current_user
-    @post_id = 1 # This part should be revised
+    # @user = User.new
+    # @current_user = @user.current_user
+    # @post_id = 1 # This part should be revised
   end
 
   # TODO: refactor this code
   def create
-    author_id = params[:author_id]
-    post_id = params[:post_id]
-    text = params[:text]
+    @comment =  Comment.new(comment_params)
+    # @author = Author.find(params[:author_id])
+    @post = Post.find(params[:post_id])
+    @comment.author = current_user
+    @comment.post = @post
+   
+    if @comment.save   
+      redirect_to(:back)
+    else
+      render :new
+    end
+  end
 
-    @comment = Comment.new(
-      author_id:,
-      post_id:,
-      text:
-    )
-    redirect_to if @comment.save
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text)
   end
 end
