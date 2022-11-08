74a1c0ed3304521577e032ebb7bafe723740ce02
wayungi <fwayungi@gmail.com>
2022-11-08 19:51:50 +0300

    UPDATES: routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,27 @@
 Rails.application.routes.draw do
-  get 'comments/new'
-  resources :posts 
-  resources :comments, only: [:create]
-  resources :likes, only: [:create, :destroy]
+  root 'users#index'
+  resources :users, only: [:index, :show]
+  # resources :posts 
+  # resources :comments, only: [:create]
+  # resources :likes, only: [:create, :destroy]
 
   # get 'posts/new'
 
 
   # temporarly root
-  root 'users#index'
-  get '/users/index', to: 'users#index'
+  # get '/users/index', to: 'users#index'
 
   # Show a single user
-  get '/users/:id', to: 'users#show' 
+  # get '/users/:id', to: 'users#show' 
 
   # Show all posts of a single user
-  get 'users/:id/posts', to: 'users#user_posts'
+  # get 'users/:id/posts', to: 'users#user_posts'
 
   # Show a single post
-  get 'users/:user_id/posts/:id', to:'users#user_post'
+  # get 'users/:user_id/posts/:id', to:'users#user_post'
 
   #==================  posts routes=======
-  get 'posts/new', to: 'posts#new'
+  # get 'posts/new', to: 'posts#new'
 
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
