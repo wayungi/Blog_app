b1d2ab5771ce45da17d57c0f2c29ac17c31f5d24
wayungi <fwayungi@gmail.com>
2022-11-08 14:12:27 +0300

    Add username test

--- spec/users_controller_spec.rb
+++ spec/users_controller_spec.rb
@@ -2,10 +2,23 @@ require 'rails_helper'
 
 
 RSpec.describe 'index page tests', type: :feature do
+    let!(:user) do
+        User.create(name: 'Gloria', photo:'https://picsum.photos/200', bio:'Swasa top loader')
+    end
+
+    let!(:post) do
+        Post.create(author_id: user.id, title: 'UCU', text: 'I am done with UCU life')
+    end
+
+    before(:each) do
+        # visit user_path(user)
+        visit root_path
+    end
+
     #     I can see the username of all other users.
     scenario 'Show usernames on the index page' do
-        visit root_path
-        expect(page).to have_content('users')
+       
+        expect(page).to have_content(user.name)
     end
 end
 
